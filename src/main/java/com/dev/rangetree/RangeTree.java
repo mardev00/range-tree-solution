c30a3fa16a9d29aa358a6d1be46fb9233a0f9adb
mardev00 <armenta.martin@gmail.com>
2017-01-30 00:07:51 -0800

    updates

--- src/main/java/com/dev/rangetree/RangeTree.java
+++ src/main/java/com/dev/rangetree/RangeTree.java
@@ -1,4 +1,4 @@
-package com.dev.wsi;
+package com.dev.rangetree;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/test/java/com/dev/rangetree/RangeTreeTest.java
+++ src/test/java/com/dev/rangetree/RangeTreeTest.java
@@ -1,6 +1,8 @@
-package com.dev.wsi;
+package com.dev.rangetree;
 
 import  org.junit.Assert;
+
+import com.dev.rangetree.RangeTree;
 
 import java.util.List;
 import org.junit.*;
@@ -72,6 +74,39 @@ public class RangeTreeTest {
 		}
 		System.out.println(rangeBuilder.toString());
 		Assert.assertEquals("[94133,94133] [94200,94299]", rangeBuilder.toString().trim());
+		System.out.println("");
+		System.out.println("-------------------------------------------------------");
+		System.out.println("-------------------------------------------------------");
+		
+	}
+	
+	@Test
+	public void testOverlappingRanges2() {
+		RangeTree rt = new RangeTree();
+
+
+		// TEST CASE 3
+		String[] range4 = { "98500","98540" };
+		String[] range5 = { "98520","98560" };
+		String[] range6 = { "98621","98635" };
+
+		String[][] testArray2 = new String[3][2];
+		testArray2[0] = range4;
+		testArray2[1] = range5;
+		testArray2[2] = range6;
+
+
+		List<String> flatRanges2 = rt.processRanges(testArray2);
+		System.out.println("-------------------------------------------------------");
+		System.out.println("OUTPUT TEST CASE 2: ");
+		System.out.println("-------------------------------------------------------");
+		StringBuilder rangeBuilder = new StringBuilder();
+		for (String range : flatRanges2) {
+			rangeBuilder.append(range);
+			System.out.print(range);
+		}
+		System.out.println(rangeBuilder.toString());
+		Assert.assertEquals("[98500,98560] [98621,98635]", rangeBuilder.toString().trim());
 		System.out.println("");
 		System.out.println("-------------------------------------------------------");
 		System.out.println("-------------------------------------------------------");
