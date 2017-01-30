d75dd26c9f9d75baa53b257d16c951155a5d2e4e
mardev00 <armenta.martin@gmail.com>
2017-01-29 23:36:43 -0800

    adding files

---
+++ src/main/java/com/dev/wsi/RangeTree.java
@@ -0,0 +1,202 @@
+package com.dev.wsi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class Node represents a range that will be stored in a BST
+ * 
+ * @author martinarmenta
+ *
+ */
+class Node {
+
+	Node(int lo, int hi, Node parent) {
+		this.lo = lo;
+		this.hi = hi;
+	}
+
+	/*
+	 * lo represents lower value in range
+	 */
+	int lo;
+
+	/**
+	 * hi represents upper value in range
+	 */
+	int hi;
+
+	Node left;
+	Node right;
+}
+
+/**
+ * Class RangeTree uses a BST data structure to store zip code ranges that in turn maybe combined
+ * for efficient storage and removal
+ * @author martinarmenta
+ *
+ */
+public class RangeTree {
+
+	/**
+	 * List of ranges that will be returned
+	 */
+	List<String> ranges;
+
+	Node root;
+
+	public RangeTree() {
+		ranges = new ArrayList<String>();
+	}
+
+	/**
+	 * Returns the ranges in order
+	 * 
+	 * @param inputArray
+	 * @return List<String>
+	 */
+	public List<String> processRanges(String[][] inputArray) {
+		for (String[] value : inputArray) {
+			// if first value then insert at root
+			if (root == null) {
+				System.out.println("root: " + value[0] + "," + value[1]);
+				root = new Node(Integer.parseInt(value[0]), Integer.parseInt(value[1]), null);
+			} else {
+				// insert rest of the values
+				insert(Integer.parseInt(value[0]), Integer.parseInt(value[1]), root);
+			}
+		}
+
+		return readRanges(root);
+	}
+
+	/**
+	 * insert method checks if range lies between the lo and hi of current node
+	 * if so it then updates the corresponding values instead of creating a new
+	 * node
+	 * 
+	 * @param x
+	 * @param y
+	 * @param current
+	 */
+	private Node insert(int x, int y, Node current) {
+		System.out.println("Inserting " + x + "," + y);
+
+		// find current node to insert
+
+		// check if values are in range of current node
+		// is there overlap in lower bound?
+		if ((x >= current.lo) && y <= current.lo) {
+			// do nothing no need to create a new node
+		} else if (y < current.lo) {
+			// add left node
+			if (current.left == null) {
+				System.out.println("Current(" + current.lo + "," + current.hi + ")->left: " + x + "," + y);
+				current.left = new Node(x, y, current);
+			} else
+				insert(x, y, current.left);
+		} else if (x > current.hi) {
+			// add right node
+			if (current.right == null) {
+				System.out.println("Current(" + current.lo + "," + current.hi + ")->right: " + x + "," + y);
+				current.right = new Node(x, y, current);
+			} else
+				insert(x, y, current.right);
+		}
+		// // first check parent
+		// else if(current.parent.lo>= x && current.parent.hi >= y && y >=
+		// current.lo) {
+		//
+		// }
+		else if (y >= current.lo && x <= current.lo) {
+			// node overlaps to the left so we need to update lower bound of
+			// range
+			System.out.println("Updating Node: " + current.lo + "," + current.hi);
+			current.lo = x;
+			System.out.println("Updated Node lo " + current.lo + "," + current.hi);
+			// visit children nodes
+			updateLeft(current);
+		} else if (x <= current.hi && y >= current.hi) {
+			// node overlaps to the right so we need to update upper bound of
+			// range
+			System.out.println("Updating Node: " + current.lo + "," + current.hi);
+			current.hi = y;
+			System.out.println("Updated Node hi " + current.lo + "," + current.hi);
+			updateRight(current);
+		}
+		return current;
+	}
+
+	/**
+	 * method updateLeft Recusively update left sub-tree
+	 * 
+	 * @param current
+	 */
+	private void updateLeft(Node current) {
+		// if left node is in range then delete and update
+		if (current == null)
+			return;
+		else {
+			if (current.left != null) {
+				// compare
+				if (current.lo <= current.left.hi) {
+					// update
+					current.lo = current.left.lo;
+				}
+				updateLeft(current.left);
+				current.left = null;
+			}
+
+		}
+
+	}
+
+	/**
+	 * method updateRight Recusively update left sub-tree
+	 * 
+	 * @param current
+	 */
+	private void updateRight(Node current) {
+		// if left node is in range then delete and update
+		if (current == null)
+			return;
+		else {
+			if (current.right != null) {
+				// compare
+				if (current.hi >= current.right.lo) {
+					// update
+					current.hi = current.right.hi;
+				}
+				updateRight(current.right);
+				current.right = null;
+			}
+
+		}
+
+	}
+
+	/**
+	 * readRanges method visits the nodes of the tree "In Order" and prints the
+	 * corresponding values
+	 * 
+	 * @param current
+	 * @return List<String>
+	 */
+	public List<String> readRanges(Node current) {
+		if (current == null)
+			return null;
+		readRanges(current.left);
+		// is there an overlap to the left?
+		// if(current)
+		ranges.add("[" + current.lo + "," + current.hi + "] ");
+		readRanges(current.right);
+		return ranges;
+	}
+
+	public static void main(String args[]) {
+		RangeTree testTree1 = new RangeTree();
+		RangeTree testTree2 = new RangeTree();
+
+	}
+
+}
---
+++ src/test/java/com/dev/wsi/RangeTreeTest.java
@@ -0,0 +1,80 @@
+package com.dev.wsi;
+
+import  org.junit.Assert;
+
+import java.util.List;
+import org.junit.*;
+
+
+public class RangeTreeTest {
+
+	/**
+	 * EXAMPLES: If the input = [94133,94133] [94200,94299] [94600,94699]
+	 * Then the output should be = [94133,94133] [94200,94299] [94600,94699]
+	 * 
+	 * If the input = [94133,94133] [94200,94299] [94226,94399] Then the
+	 * output should be = [94133,94133] [94200,94399]
+	 */
+	@Test
+	public void testRangesNotOverlapping() {
+		RangeTree rt = new RangeTree();
+
+		// TEST CASE 1
+		String[] range1 = { "94133", "94133" };
+		String[] range2 = { "94200", "94299" };
+		String[] range3 = { "94600", "94699" };
+		String[][] testArray1 = new String[3][2];
+		testArray1[0] = range1;
+		testArray1[1] = range2;
+		testArray1[2] = range3;
+
+		
+		List<String> flatRanges = rt.processRanges(testArray1);
+		System.out.println("-------------------------------------------------------");
+		System.out.println("OUTPUT TEST CASE 1: ");
+		StringBuilder rangeBuilder = new StringBuilder();
+		for (String range : flatRanges) {
+			rangeBuilder.append(range);
+			System.out.print(range);
+		}
+		System.out.println("");
+		System.out.println("-------------------------------------------------------");
+		System.out.println("-------------------------------------------------------");
+		Assert.assertEquals("[94133,94133] [94200,94299] [94600,94699]", rangeBuilder.toString().trim());
+		
+	}
+	
+	@Test
+	public void testOverlappingRanges() {
+		RangeTree rt = new RangeTree();
+
+
+
+		// TEST CASE 2
+		String[] range4 = { "94133", "94133" };
+		String[] range5 = { "94200", "94299" };
+		String[] range6 = { "94200", "94399" };
+
+		String[][] testArray2 = new String[3][2];
+		testArray2[0] = range4;
+		testArray2[1] = range5;
+		testArray2[2] = range6;
+
+
+		List<String> flatRanges2 = rt.processRanges(testArray2);
+		System.out.println("-------------------------------------------------------");
+		System.out.println("OUTPUT TEST CASE 2: ");
+		System.out.println("-------------------------------------------------------");
+		StringBuilder rangeBuilder = new StringBuilder();
+		for (String range : flatRanges2) {
+			rangeBuilder.append(range);
+			System.out.print(range);
+		}
+		System.out.println(rangeBuilder.toString());
+		Assert.assertEquals("[94133,94133] [94200,94299]", rangeBuilder.toString().trim());
+		System.out.println("");
+		System.out.println("-------------------------------------------------------");
+		System.out.println("-------------------------------------------------------");
+		
+	}
+}
